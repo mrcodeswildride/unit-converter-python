b69f932a31997df3cc4fb84cac0897c79548311c
David Keller <kellerdb@gmail.com>
2021-12-09 09:19:55 -0800

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+print()
+
+feet = float(input("Enter a length in feet: "))
+inches = feet * 12
+print(f"{feet} feet is {inches} inches.\n")
+
+hours = float(input("Enter a duration in hours: "))
+minutes = hours * 60
+print(f"{hours} hours is {minutes} minutes.\n")
+
+c = float(input("Enter a temperature in Celsius: "))
+f = (c * 9/5) + 32
+print(f"{c} degrees Celsius is {f} degrees Fahrenheit.")
